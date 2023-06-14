5b39c0957ab32fffdf92c30a51de260f9a05457d
parisasetvati <p.s.setvati@gmail.com>
2023-06-15 00:07:31 +0330

    chckout page modify

--- src/Component/Pages/checkOut/Check.js
+++ src/Component/Pages/checkOut/Check.js
@@ -1,10 +1,15 @@
-const Check= () => {
-
-    return ( 
-      
-   <h4>this is checkout page!</h4>
-    
+const Check = () => {
+  return (
+    <section>
+      <h4>this is checkout page!</h4>
+      <div>
+        <spa>Product List</spa>
+      </div>
+      <div>
+        <span>Discount Product</span>
+      </div>
+    </section>
   );
-}
- 
+};
+
 export default Check;
